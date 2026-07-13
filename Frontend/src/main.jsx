9f3a84fb4d948b3fda8b1c2df3d80bb6e9f5f8d3
Utkarsh7jha7 <utkarsh7jha7@gmail.com>
2026-07-13 18:05:19 +0530

    Tailwind update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App";
+import "./index.css";   // ✅ THIS LINE WAS MISSING
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <BrowserRouter>
-
       <App />
 
       <Toaster
@@ -18,8 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           duration: 3000,
         }}
       />
-
     </BrowserRouter>
-
   </React.StrictMode>
 );
